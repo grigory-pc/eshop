5c488c1f06f5c71f09fc3f020ad798f9d80fe7ce
Grigory <grigoriy.trubnikov@ebs.ru>
2025-07-20 14:07:40 +0300

    feat: обновление конфигурации oauth2

--- core-service/src/main/java/ru/yandex/practicum/eshop/core/config/security/OAuth2ClientConfiguration.java
+++ core-service/src/main/java/ru/yandex/practicum/eshop/core/config/security/OAuth2ClientConfiguration.java
@@ -1,35 +1,43 @@
-//package ru.yandex.practicum.eshop.core.config.security;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
-//import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
-//import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
-//import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-//import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-//import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
-//
-//@Configuration
-//public class OAuth2ClientConfiguration {
-//
-//  @Bean
-//  public OAuth2AuthorizedClientManager authorizedClientManager(
-//      ClientRegistrationRepository clientRegistrationRepository,
-//      OAuth2AuthorizedClientService authorizedClientService) {
-//
-//    AuthorizedClientServiceOAuth2AuthorizedClientManager manager =
-//        new AuthorizedClientServiceOAuth2AuthorizedClientManager(clientRegistrationRepository,
-//                                                                 authorizedClientService);
-//
-//    manager.setAuthorizedClientProvider(OAuth2AuthorizedClientProviderBuilder.builder()
-//                                                                             .clientCredentials()
-//                                                                             .build());
-//    return manager;
-//  }
-//
-//  @Bean
-//  public ServerOAuth2AuthorizedClientExchangeFilterFunction oauth2Client(
-//      OAuth2AuthorizedClientManager authorizedClientManager) {
-//    return new ServerOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
-//  }
-//}
+package ru.yandex.practicum.eshop.core.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
+
+@Configuration
+public class OAuth2ClientConfiguration {
+
+  @Bean
+  public ReactiveOAuth2AuthorizedClientManager authorizedClientManager(
+      ReactiveClientRegistrationRepository clientRegistrationRepository,
+      ReactiveOAuth2AuthorizedClientService authorizedClientService) {
+
+    AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager manager =
+        new AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager(
+            clientRegistrationRepository, authorizedClientService);
+
+    manager.setAuthorizedClientProvider(ReactiveOAuth2AuthorizedClientProviderBuilder.builder()
+                                                                                     .clientCredentials()
+                                                                                     .build());
+
+    return manager;
+  }
+
+  @Bean
+  public ServerOAuth2AuthorizedClientExchangeFilterFunction oauth2Client(
+      ReactiveOAuth2AuthorizedClientManager authorizedClientManager) {
+
+    ServerOAuth2AuthorizedClientExchangeFilterFunction oauth2Client =
+        new ServerOAuth2AuthorizedClientExchangeFilterFunction(
+            authorizedClientManager);
+
+    oauth2Client.setDefaultOAuth2AuthorizedClient(true);
+
+    return oauth2Client;
+  }
+}
